11242d1a921b488ce33771d99e6c2ba77cf15921
Brynn Chapman <brynn_m_chapman@outlook.com>
2019-11-07 22:50:55 -0600

    Minor edits, renamed a file.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,21 +1,20 @@
-//Initialize Firebase database
-// Initialize Firebase
-var config = {
-	apiKey: "AIzaSyAJgJDtXUu5e5dwzeDVF5Hw5dnmcZwyV5s",
-    authDomain: "fir-train-scheduler-7f4a9.firebaseapp.com",
-    databaseURL: "https://fir-train-scheduler-7f4a9.firebaseio.com",
-    projectId: "fir-train-scheduler-7f4a9",
-    storageBucket: "",
-    messagingSenderId: "123683577611"
- };
- firebase.initializeApp(config);
+var firebaseConfig = {
+    apiKey: "AIzaSyCCuuHhpYVMyj7PRFsrDcjaNra_A1ge5XM",
+    authDomain: "train-de400.firebaseapp.com",
+    databaseURL: "https://train-de400.firebaseio.com",
+    projectId: "train-de400",
+    storageBucket: "train-de400.appspot.com",
+    messagingSenderId: "192237766993",
+    appId: "1:192237766993:web:a82d5c8bbaa7f03f72ab70"
+  };
 
- //Create database variable to create reference to firebase.database().
+  firebase.initializeApp(firebaseConfig);
+
  var database = firebase.database();
 
  var tMinutesTillTrain = 0;
 
-//Show and update current time. Use setInterval method to update time.
+
 function displayRealTime() {
 setInterval(function(){
     $('#current-time').html(moment().format('hh:mm A'))
@@ -27,33 +26,28 @@ displayRealTime();
  var tRow = "";
  var getKey = "";
 
- //Click event for the submit button. When user clicks Submit button to add a train to the schedule...
  $("#submit-button").on("click", function() {
 
-	// Prevent form from submitting
 	event.preventDefault();
 
-	//Grab the values that the user enters in the text boxes in the "Add train" section. Store the values in variables.
 	var trainName = $("#train-name").val().trim();
 	var destination = $("#destination").val().trim();
 	var firstTrainTime = $("#first-train-time").val().trim();
 	var trainFrequency = $("#frequency").val().trim();
 
-	//Print the values that the user enters in the text boxes to the console for debugging purposes.
+
 	console.log(trainName);
 	console.log(destination);
 	console.log(firstTrainTime);
 	console.log(trainFrequency);
 
-	//Form validation for user input values. To add a train, all fields are required.
-	//Check that all fields are filled out.
 	if (trainName === "" || destination === "" || firstTrainTime === "" || trainFrequency === ""){
 		$("#not-military-time").empty();
 		$("#missing-field").html("ALL fields are required to add a train to the schedule.");
 		return false;		
 	}
 
-	//Check to make sure that there are no null values in the form.
+	
 	else if (trainName === null || destination === null || firstTrainTime === null || trainFrequency === null){
 		$("#not-military-time").empty();
 		$("#not-a-number").empty();
@@ -61,7 +55,6 @@ displayRealTime();
 		return false;		
 	}
 
-	//Check that the user enters the first train time as military time.
 	else if (firstTrainTime.length !== 5 || firstTrainTime.substring(2,3)!== ":") {
 		$("#missing-field").empty();
 		$("#not-a-number").empty();
@@ -69,7 +62,6 @@ displayRealTime();
 		return false;
 	}
 
-	//Check that the user enters a number for the Frequency value.
 	else if (isNaN(trainFrequency)) {
     	$("#missing-field").empty();
     	$("#not-military-time").empty();
@@ -77,38 +69,29 @@ displayRealTime();
     	return false;
 	}
 
-	//If form is valid, perform time calculations and add train to the current schedule.
 	else {
 		$("#not-military-time").empty();
 		$("#missing-field").empty();
 		$("#not-a-number").empty();
 
-		//Moment JS math caclulations to determine train next arrival time and the number of minutes away from destination.
-		// First Time (pushed back 1 year to make sure it comes before current time)
 	    var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
 	    console.log(firstTimeConverted);
 
-	    // Current Time
 	    var currentTime = moment();
 	    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-	    // Difference between the times
 	    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 	    console.log("DIFFERENCE IN TIME: " + diffTime);
 
-	    // Time apart (remainder)
 	    var tRemainder = diffTime % trainFrequency;
 	    console.log(tRemainder);
 
-	    // Minute Until Train
 	    var tMinutesTillTrain = trainFrequency - tRemainder;
 	    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-	    // Next Train
 	    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm A");
 	    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-		//Create local temporary object for holding train data
 		var newTrain = {
 			trainName: trainName,
 			destination: destination,
@@ -119,10 +102,8 @@ displayRealTime();
 			currentTime: currentTime.format("hh:mm A")
 		};
 
-		//Save/upload train data to the database.
 		database.ref().push(newTrain);
 
-		//Log everything to console for debugging purposes.
 		console.log("train name in database: " + newTrain.trainName);
 		console.log("destination in database: " + newTrain.destination);
 		console.log("first train time in database: " + newTrain.firstTrainTime);
@@ -131,11 +112,10 @@ displayRealTime();
 		console.log("minutes away in database: " + newTrain.tMinutesTillTrain);
 		console.log("current time in database: " + newTrain.currentTime);
 
-		//Confirmation modal that appears when user submits form and train is added successfully to the schedule.
 		$(".add-train-modal").html("<p>" + newTrain.trainName + " was successfully added to the current schedule.");
 		$('#addTrain').modal();
 
-		//Remove the text from the form boxes after user presses the add to schedule button.
+		
 		$("#train-name").val("");
 		$("#destination").val("");
 		$("#first-train-time").val("");
@@ -144,13 +124,10 @@ displayRealTime();
 });
 
 
-// At the initial load and subsequent value changes, get a snapshot of the stored data.
-// This function allows you to update the page in real-time when the firebase database changes.
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 	console.log(childSnapshot.val());
 	console.log(prevChildKey);
 
-	//Set variables for form input field values equal to the stored values in firebase.
 	var trainName = childSnapshot.val().trainName;
 	var destination = childSnapshot.val().destination;
 	var firstTrainTime = childSnapshot.val().firstTrainTime;
@@ -159,7 +136,7 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 	var tMinutesTillTrain = childSnapshot.val().tMinutesTillTrain;
 	var currentTime = childSnapshot.val().currentTime;
 
-	//Train info
+
 	console.log(trainName);
 	console.log(destination);
 	console.log(firstTrainTime);
@@ -168,33 +145,25 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 	console.log(trainFrequency);
 	console.log(currentTime);
 
-	//Moment JS math caclulations to determine train next arrival time and the number of minutes away from destination.
-	// First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
     console.log(firstTimeConverted);
 
-    // Current Time
     var currentTime = moment();
     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-    // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     console.log("DIFFERENCE IN TIME: " + diffTime);
 
-    // Time apart (remainder)
     var tRemainder = diffTime % trainFrequency;
     console.log(tRemainder);
 
-    // Minute Until Train
+   
     var tMinutesTillTrain = trainFrequency - tRemainder;
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-    // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm A");
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-
-	//Update the HTML (schedule table) to reflect the latest stored values in the firebase database.
 	var tRow = $("<tr>");
 	var trainTd = $("<td>").text(trainName);
     var destTd = $("<td>").text(destination);
@@ -202,33 +171,23 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
     var trainFrequencyTd = $("<td>").text(trainFrequency);
     var tMinutesTillTrainTd = $("<td>").text(tMinutesTillTrain);
 
-    // Append the newly created table data to the table row.
-    //Append trash can icon to each row so that user can delete row if needed.
     tRow.append("<img src='assets/images/binart.png' alt='trash can' class='trash-can mr-3'>", trainTd, destTd, trainFrequencyTd, nextTrainTd, tMinutesTillTrainTd);
-    // Append the table row to the table body
+   
     $("#schedule-body").append(tRow);
 });
 
 
-//Click event for trash can icon/button. When user clicks trash can to remove a train from the schedule...
+
  $("body").on("click", ".trash-can", function(){
-	// Prevent form from submitting
+	
 	event.preventDefault();
 
-	//confirm with the user before he or she decides to actually delete the train data.
 	var confirmDelete = confirm("Deleting a train permanently removes the train from the system. Are you sure you want to delete this train?");
-	//To do: Replace alert with modal... Confirmation modal that appears when user wants to remove train from schedule.
-		//$(".remove-train-modal").html("<p>" + " Deleting a train permanently removes the train from the system. Are you sure you want to delete this train?" + "</p>");
-		//$('#removeTrain').modal();
-	//If user confirms...
+
 	if (confirmDelete) {
-		//Remove the closest table row.
+		
 		$(this).closest('tr').remove();
-		//To do: Remove train info from db.
-		// getKey = $(this).parent().attr('id');
-		// console.log(getKey);
-		//dataRef.child(key).remove();
-		//database.ref().child(getKey).remove();
+	
 	}
 
 	else {
@@ -236,7 +195,6 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 	}
 });
 
-//One way to initialize all tooltips on a page would be to select them by their data-toggle attribute:
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
